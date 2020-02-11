e793155ec8569f1d41830300ce43db1469c4bd0f
byazit@gmail.com <byazit@gmail.com>
2020-02-11 10:48:53 +0100

    routing

---
+++ routing/src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './App.css';
+import Nav from './Nav';
+import About from './About';
+import Shop from './Shop';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">      
+        <Nav />
+        <switch>
+          <Route path='/' exact component={Home} />
+          <Route path='/about' component={About} />
+          <Route path='/shop' component={Shop} />
+        </switch>
+      </div>
+    </Router>
+  );
+}
+const Home=()=>(
+  <div>
+    <h1>Home page</h1>
+  </div>
+);
+export default App;
