cff757d77ebb24d788a75901414117c5627935a5
ElinHallengren <elinhallengren@gmail.com>
2020-02-17 11:46:56 +0100

    changed size and color of header

--- create-react-app-express/client/src/components/ButtonAppBar.js
+++ create-react-app-express/client/src/components/ButtonAppBar.js
@@ -8,6 +8,7 @@ import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import { MemoryRouter as Router } from 'react-router';
 import { Link } from 'react-router-dom';
+import { AvMicNone } from 'material-ui/svg-icons';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -19,6 +20,7 @@ const useStyles = makeStyles(theme => ({
   title: {
     flexGrow: 1,
   },
+  
 }));
 
 export default function ButtonAppBar() {
@@ -31,8 +33,8 @@ export default function ButtonAppBar() {
           <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
             <MenuIcon />
           </IconButton>
-          <Typography variant="h6" className={classes.title}>
-            <a href='/'>Store Name</a>
+          <Typography variant="h4" className={classes.title}>
+            <a style={{ textDecoration: 'none', color: '#fff' }} href='/'>Store Name</a>
           </Typography>
          
           <Button color="inherit" href="/newUser">Add New User</Button>
